c5fb7b90383c08efd31a19d00de61995da1828ab
dantoso <xeddantasvfd@gmail.com>
2022-05-11 12:15:07 -0300

    a

--- tvGame/AppDelegate.swift
+++ tvGame/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SpriteKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
+		let vc = GameViewController()
+		window = UIWindow(frame: UIScreen.main.bounds)
+		window?.rootViewController = vc
+		window?.makeKeyAndVisible()
 		return true
 	}
 
