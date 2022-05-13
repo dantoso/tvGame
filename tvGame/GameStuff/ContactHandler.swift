34821299595f190fa6785ea29556558282918faa
dantoso <xeddantasvfd@gmail.com>
2022-05-12 21:38:41 -0300

    Update ContactHandler.swift

--- tvGame/GameStuff/ContactHandler.swift
+++ tvGame/GameStuff/ContactHandler.swift
@@ -6,6 +6,7 @@ extension GameScene: SKPhysicsContactDelegate {
 		guard let size = leftPlayer?.frame.size else {fatalError()}
 		removeChildren(in: [disk!])
 		disk = createDisk(radius: size.width*0.5)
+		addChild(disk!)
 	}
 	
 }
