282fc48a1af7dfb7b68c2613cb9d9e53a7956bf7
dantoso <xeddantasvfd@gmail.com>
2023-02-10 18:43:30 -0300

    feat: implementing p2p connector

--- tvGame/Controller/GameViewController.swift
+++ tvGame/Controller/GameViewController.swift
@@ -1,19 +1,15 @@
 import SpriteKit
 import MultipeerConnectivity
+import SwiftP2PConnector
 
-class GameViewController: UIViewController {
+final class GameViewController: UIViewController, ReceiveDelegate {
 
-	lazy var id = MCPeerID(displayName: UIDevice.current.name)
-	lazy var mcSession = MCSession(peer: id, securityIdentity: nil, encryptionPreference: .required)
-	lazy var advertiser = MCAdvertiserAssistant(serviceType: "mdv-hm", discoveryInfo: nil, session: mcSession)
-	
 	lazy var scene = GameScene(size: view.bounds.size)
+	var commandDictionary: [String : Command] = [:]
 	
     override func viewDidLoad() {
         super.viewDidLoad()
-		
-		mcSession.delegate = self
-		
+				
 		view = SKView(frame: view.bounds)
 		
 		// Set the scale mode to scale to fit the window
@@ -32,7 +28,17 @@ class GameViewController: UIViewController {
 			view.presentScene(scene)
 		}
 		
-		startHosting()
+		P2PConnector.startHosting()
 	}
 
+	func didReceiveKey(_ key: String, from peerID: MCPeerID) {}
+	
+	func didReceiveData(_ data: Data, from peerID: MCPeerID) {
+		if scene.player1.id == peerID {
+			scene.player1.processData(data)
+		}
+		else {
+			scene.player2.processData(data)
+		}
+	}
 }
--- tvGame/Controller/P2P.swift
+++ tvGame/Controller/P2P.swift
@@ -1,112 +1,43 @@
 import SpriteKit
 import MultipeerConnectivity
+import SwiftP2PConnector
 
-extension GameViewController: MCSessionDelegate {
-	
-	func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
-		switch state {
-		case .notConnected:
-			if scene.player1.id == peerID {
-				scene.player1.id = nil
-			}
-			else if scene.player2.id == peerID {
-				scene.player2.id = nil
-			}
-			print("\(peerID.displayName): Disconnected")
-			
-		case .connecting:
-			print("\(peerID.displayName): Connecting...")
-			
-		case .connected:
-			if scene.player1.id == nil {
-				scene.player1.id = peerID
-				
-				let data = encodeColor(.systemPurple)
-				sendData(data, to: [peerID])
-				
-				print("\(peerID.displayName): Connected to left player!")
-			}
-			
-			else if scene.player2.id == nil {
-				scene.player2.id = peerID
-
-				let data = encodeColor(.systemGreen)
-				sendData(data, to: [peerID])
-				
-				print("\(peerID.displayName): Connected to right player!")
-			}
-			
-			if mcSession.connectedPeers.count > 2 {
-				advertiser.stop()
-			}
-			
-		@unknown default:
-			print("some weird shit just happend")
-		}
-	}
-	
-	func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
-		if scene.player1.id == peerID {
-			scene.player1.processData(data)
-		}
-		else if scene.player2.id == peerID {
-			scene.player2.processData(data)
-		}
-	}
-	
-	func startHosting() {
-		advertiser.start()
-		print("hosting started")
-	}
-	
-	func encodeColor(_ color: UIColor) -> Data? {
-		let data = try? NSKeyedArchiver.archivedData(withRootObject: color, requiringSecureCoding: false) as Data?
-		return data
-	}
-	
-	func sendData(_ data: Data?, to peers: [MCPeerID]) {
-		
-		guard mcSession.connectedPeers.count > 0 else {return}
-		guard let realData = data else {return}
-		
-		do {
-			try mcSession.send(realData, toPeers: peers, with: .reliable)
-			print("data from \(id.displayName) sent")
-		}
-		catch let error as NSError {
-		   let ac = UIAlertController(title: "Send error", message: error.localizedDescription, preferredStyle: .alert)
-		   ac.addAction(UIAlertAction(title: "OK", style: .default))
-		   present(ac, animated: true)
-	   }
-		
-	}
-	
-	func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
-		
-	}
-	
-	func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
-		
-	}
-	
-	func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
-		
-	}
-	
+enum CommandKeys: String {
+	case changeColorToGreen
+	case changeColorToPurple
 }
 
-extension GameViewController: MCBrowserViewControllerDelegate {
+extension GameViewController: ConnectionDelegate {
 	
-//	func browserViewController(_ browserViewController: MCBrowserViewController, shouldPresentNearbyPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) -> Bool {
-//		<#code#>
-//	}
-//
-	func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
-		dismiss(animated: true, completion: nil)
+	func didDisconnect(with peerID: MCPeerID) {
+		if scene.player1.id == peerID {
+			scene.player1.id = nil
+		}
+		else if scene.player2.id == peerID {
+			scene.player2.id = nil
+		}
+		print("\(peerID.displayName): Disconnected")
 	}
 	
-	func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
-		dismiss(animated: true, completion: nil)
+	func isConnecting(with peerID: MCPeerID) {
+		print("\(peerID.displayName): Connecting...")
 	}
 	
+	func didConnect(with peerID: MCPeerID) {
+		if scene.player1.id == nil {
+			scene.player1.id = peerID
+			P2PConnector.sendKey(CommandKeys.changeColorToGreen.rawValue, to: [peerID])
+			print("\(peerID.displayName): Connected to left player!")
+		}
+		
+		else if scene.player2.id == nil {
+			scene.player2.id = peerID
+			P2PConnector.sendKey(CommandKeys.changeColorToPurple.rawValue, to: [peerID])
+			print("\(peerID.displayName): Connected to right player!")
+		}
+		
+		if P2PConnector.connectedPeers.count > 2 {
+			P2PConnector.stopHosting()
+		}
+	}
 }
